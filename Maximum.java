159889de09f0661d053ec38aa288f339cbe1315e
Karthik <jeefkarthik036@gmail.com>
2022-04-04 19:55:18 +0530

    added-UC3

--- Maximum.java
+++ Maximum.java
@@ -2,13 +2,13 @@ package com.bridge;
 
 public class Maximum {
 
-	public static Float getmaximum(Float value1, Float value2, Float value3) {
-        Float max = value1;
-        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
-            max = value2;
-        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
-            max = value3;
-        System.out.println(max);
-        return max;
+	 public static String getmaximum(String value1, String value2, String value3) {
+	        String max = value1;
+	        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+	            max = value2;
+	        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+	            max = value3;
+	        System.out.println(max);
+	        return max;
     }
 }
